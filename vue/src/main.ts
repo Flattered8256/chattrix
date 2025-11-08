d78f77accb9bbb87dbade4164e40b798ff517a9c
zhaojingnan <2026617199@qq.com>
2025-11-08 17:50:01 +0800

    集成了Element UI

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -4,9 +4,14 @@ import router from "./router";
 import "./styles/global.scss"; // 导入全局样式
 import App from "./App.vue";
 
+// 引入 Element Plus
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
+app.use(ElementPlus); // 注册 Element Plus
 app.mount("#app");
