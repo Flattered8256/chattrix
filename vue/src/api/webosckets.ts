05e14513bf2d5a1275374bbe8abe924762cbdaea
Flattered8256 <Flattered8256@outlook.com>
2025-11-11 18:27:24 +0800

    更新了域名

--- vue/src/api/webosckets.ts
+++ vue/src/api/webosckets.ts
@@ -240,7 +240,7 @@ export class WebSocketService {
   // 获取WebSocket URL
   private getWsUrl(endpoint: string): string {
     // 将HTTP URL转换为WebSocket URL
-    return `wss://xenial-muskox-chattrix-c42d8bd9.koyeb.app/${endpoint}`;
+    return `wss://practical-becky-chattrix-f0677ddd.koyeb.app/${endpoint}`;
   }
 
   // 创建或获取聊天室WebSocket连接
